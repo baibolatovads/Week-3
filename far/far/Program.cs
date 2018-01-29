c271f8ec8286bdf739c74c09678987ae60ee6679
Baibolatova Dariya <baibolatovads@gmail.com>
2018-01-29 11:50:39 +0600

    first commit

---
+++ far/far/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace far
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Environment.GetLogicalDrives();
+            Console.SetWindowSize(40, 40);
+            FAR far = new FAR(@"C:\Users\User_PC\Documents\PP2");
+
+            bool quit = false;
+
+            while (!quit)
+            {
+                far.Draw();
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.Escape:
+                        quit = true;
+                        break;
+                    case ConsoleKey.UpArrow:
+                    case ConsoleKey.DownArrow:
+                    case ConsoleKey.Backspace:
+                    case ConsoleKey.Enter:
+                        far.Process(pressedKey);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
